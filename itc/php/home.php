faf71ff90a02259f1442e81b77733a0f99225e40
rickipsgx <rickipsgx@hotmail.com>
2012-12-21 17:56:17 -0500

    modelo de la pagina :D
    Este es el primer modelo de la pagina de ITC

---
+++ itc/php/home.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8"/>
+	<title>Home | Ingenia Tu Codigo</title>
+	<meta name="viewport" conten="width=device-width,initial-scale=1"/>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css"/>
+	<link rel="stylesheet" type="text/css" href="css/style_alt.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script>
+        !window.jQuery && document.write("<script src='js/jquery.min.js'><\/script>");
+    </script>
+	<script type="text/javascript" src="js/jquery.jmslideshow.js"></script>
+	<script type="text/javascript" src="js/jmpress.js"></script>
+		<!-- jmslideshow plugin : extends the jmpress plugin -->
+</head>
+<body>
+	<?php include("header.php"); ?>
+	<div id="slider">
+	<section id="jms-slideshow" class="jms-slideshow">
+		<div class="step" data-color="color-1" data-x="2000" data-y="1000" data-z="3000" data-rotate="-20">
+			<div class="jms-content">
+				<h3>Just when I thought...</h3>
+				<p>From fairest creatures we desire increase, that thereby beauty's rose might never die</p>
+				<a class="jms-link" href="#">Leer mas</a>
+			</div>
+			<img src="images/1.png" />
+			</div>
+			<div class="step" data-color="color-2" data-x="1000" data-z="2000" data-rotate="20">
+				<div class="jms-content">
+					<h3>Holy cannoli!</h3>
+					<p>But as the riper should by time decease, his tender heir might bear his memory</p>
+					<a class="jms-link" href="#">Leer mas</a>
+				</div>
+				<img src="images/2.png" />
+			</div>
+			<div class="step" data-color="color-3" data-x="2000" data-y="1500" data-z="1000" data-rotate="20">
+				<div class="jms-content">
+					<h3>No time to waste</h3>
+					<p>Within thine own bud buriest thy content and, tender churl, makest waste in niggarding</p>
+					<a class="jms-link" href="#">Leer mas</a>
+				</div>
+				<img src="images/3.png" />
+			</div>
+			<div class="step" data-color="color-4" data-x="3000" data-y="2000">
+				<div class="jms-content">
+					<h3>Supercool!</h3>
+					<p>Making a famine where abundance lies, thyself thy foe, to thy sweet self too cruel</p>
+					<a class="jms-link" href="#">Leer mas</a>
+				</div>
+				<img src="images/4.png" />
+			</div>
+			<div class="step" data-color="color-5" data-x="4000" data-y="1500" data-z="1000" data-rotate="-20">
+				<div class="jms-content">
+					<h3>Did you know that...</h3>
+					<p>Thou that art now the world's fresh ornament and only herald to the gaudy spring</p>
+					<a class="jms-link" href="#">Leer mas</a>
+				</div>
+				<img src="images/5.png" />
+			</div>
+		</section>
+    </div>
+    <script type="text/javascript">
+    $(window).load(function() {
+        $('#jms-slideshow').jmslideshow();
+    });
+    </script>
+</body>
+</html>
