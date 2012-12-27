0c44868b07227e51ce33ae39fc23418d22e9d68e
rickipsgx <rickipsgx@hotmail.com>
2012-12-27 18:56:44 -0500

    aumentada la parte inicial de tutoriales
    aumente la parte tutoriales.html

---
+++ itc/ITC/js/slider.js
@@ -0,0 +1,23 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<!-- jmpress plugin ->
+<script type="text/javascript" src="js/jmpress.min.js"></script>
+<!- jmslideshow plugin : extends the jmpress plugin -->
+<script type="text/javascript" src="js/jquery.jmslideshow.js"></script>
+<script type="text/javascript" src="js/modernizr.custom.48780.js"></script>
+<noscript>
+ <style>
+ 	.step {
+ 	 width: 100%;
+ 	 position: relative;
+ 	}
+ 	.step:not(.active) {
+ 	 opacity: 1;
+ 	 filter: alpha(opacity=99);
+ 	 -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(opacity=99)";
+ 	}
+ 	.step:not(.active) a.jms-link{
+ 	 opacity: 1;
+ 	 margin-top: 40px;
+ 	}
+ </style>
+</noscript>
